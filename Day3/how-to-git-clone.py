35ead17ba3d710137d9f14ac675fffcdda2a102c
akanshabaishwade <akanshabaishwade@gmail.com>
2024-08-25 21:05:57 +0530

    [add] git pull day 3

---
+++ Day3/how-to-git-clone.py
@@ -0,0 +1,17 @@
+'''
+Open your terminal and navigate to the directory of your Git repository.
+
+Run the following command:
+
+bash
+Copy code
+git pull
+This command will fetch the latest changes from the remote repository and merge them into your current branch.
+
+Detailed Explanation:
+
+git pull is a combination of two commands:
+git fetch: Retrieves the latest changes from the remote repository.
+git merge: Merges the fetched changes into your current branch.
+
+'''
